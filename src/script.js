d4749044f4bba9c4f430ae6b25a1df55c29a3538
Julian Baumann <git@julba.de>
2022-03-19 19:18:08 +0100

    Added dynamic version links

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ const linuxVersionElement = document.getElementById("linuxVersion");
 const macVersionElement = document.getElementById("macVersion");
 const windowsVersionElement = document.getElementById("windowsVersion");
 
+const linuxDownloadButton = document.getElementById("linuxDownloadButton");
+const macDownloadButton = document.getElementById("macDownloadButton");
+const windowsDownloadButton = document.getElementById("windowsDownloadButton");
+
 async function setDesktopVersionLabels() {
     const json = await fetch("downloads/desktop-versions.json").then((response) => response.json());
 
@@ -9,6 +13,10 @@ async function setDesktopVersionLabels() {
         linuxVersionElement.innerHTML = json["linux"];
         macVersionElement.innerHTML = json["macOS"];
         windowsVersionElement.innerHTML = json["windows"];
+
+        linuxDownloadButton.href = `downloads/linux-binary/InterShare-${json["linux"]}.tar.gz`;
+        macDownloadButton.href = `downloads/macOS/InterShare-${json["macOS"]}.zip`;
+        windowsDownloadButton.href = `downloads/windows/InterShare-${json["windows"]}.zip`;
     }
 }
 
